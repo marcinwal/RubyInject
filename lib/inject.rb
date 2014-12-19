b74544541531a26cf1722f5ba74c84aa8cba1801
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-19 16:27:10 +0000

    another version

--- lib/inject.rb
+++ lib/inject.rb
@@ -44,5 +44,25 @@ class Array
     end
   end
 
+  def new_inject(*args,&block)
+   if block_given? 
+     i = args.size == 0 ? 1 : 0
+     init = args[0] || self[0]
+     while self[i] 
+       init = block.call(init,self[i])
+       i += 1
+     end
+   else
+     i = args.size == 1 ? 1 : 0
+     operator = args.size == 1 ? args[0] : args[1]
+     init = args.size == 1 ? self[0] : args[0]
+     while self[i]
+      init = init.send(operator,self[i])
+      i += 1
+     end
+   end
+   return init
+  end  
+
 
 end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -30,4 +30,20 @@ describe Array do
       expect([1,2,3,4].my_inject(10,:+)).to eq(20)
     end
 
+    it 'should work with new_inject' do
+      expect([1,2,3,4].new_inject{|m,el| m + el}).to eq(10)
+    end  
+
+    it 'should work with new_inject and * ' do
+      expect([1,2,3,4].new_inject{|m,el| m * el}).to eq(24)
+    end  
+
+    it 'should work with new_inject at symbol' do
+      expect([1,2,3,4].new_inject(:*)).to eq(24)
+    end
+
+    it 'should work with new_inject , symbol and initial' do
+      expect([1,2,3,4].new_inject(10,:*)).to eq(240)
+    end
+
 end
