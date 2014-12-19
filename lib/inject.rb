f0e0c2cf800e932d5a2d537c803e11a8e06ab484
marcin walendzik <marcin.walendzik@gmail.com>
2014-12-19 16:58:10 +0000

    final version with two solutions

--- lib/inject.rb
+++ lib/inject.rb
@@ -23,26 +23,26 @@ class Array
   end
 
 
-  def fun_inject(arg = nil,arg2 = nil) 
-    if block_given? 
-      i = arg == nil ? 1 : 0
-      init = arg || self[0] * self[0]
-      while(self[i])
-        init = yield init, self[i] ,self[i]*self[i]
-        i += 1
-      end
-      return init   
-    else 
-      i = arg2 == nil ? 1 : 0
-      operator = arg2 == nil ? arg : arg2
-      init = arg2 == nil ? self[0] : arg
-      while (self[i])
-        init = init.send(operator,self[i])
-        i += 1
-      end  
-      return init
-    end
-  end
+  # def fun_inject(arg = nil,arg2 = nil) 
+  #   if block_given? 
+  #     i = arg == nil ? 1 : 0
+  #     init = arg || self[0] * self[0]
+  #     while(self[i])
+  #       init = yield init, self[i] ,self[i]*self[i]
+  #       i += 1
+  #     end
+  #     return init   
+  #   else 
+  #     i = arg2 == nil ? 1 : 0
+  #     operator = arg2 == nil ? arg : arg2
+  #     init = arg2 == nil ? self[0] : arg
+  #     while (self[i])
+  #       init = init.send(operator,self[i])
+  #       i += 1
+  #     end  
+  #     return init
+  #   end
+  # end
 
   def new_inject(*args,&block)
    if block_given? 
@@ -63,6 +63,4 @@ class Array
    end
    return init
   end  
-
-
 end
